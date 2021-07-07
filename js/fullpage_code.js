e8e8d4648c72cab0663244213210f61fb6bb539f
yunjungheo <ggomi7900@gmail.com>
2021-07-07 23:06:22 +0900

    0707 반응형 수정

--- js/fullpage_code.js
+++ js/fullpage_code.js
@@ -1,5 +1,17 @@
 $(function(){
  $("#fullpage").fullpage({
-   anchors : ['1st','2nd','3rd'],
+
+
+  navigation:true,
+  fixedElements:'#header',
+  verticalCentered:false,
+  responsiveWidth:700,
+  anchors : ['1st','2nd','3rd'],
+  afterLoad:function(anchorLink, index){
+    if(anchorLink == '2nd'){
+      $(".page_txt_box .main_txt").addClass('txt');
+      $(".page_txt_box .page_logo").addClass('txt_up');
+    }
+  }
  });
 });

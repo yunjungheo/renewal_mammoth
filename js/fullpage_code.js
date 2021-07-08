42332f54d3cc5db7da915eb2482e186fde572c76
yunjungheo <ggomi7900@gmail.com>
2021-07-08 16:57:24 +0900

    0708 반응형 수정 ,풀페이지수정

--- js/fullpage_code.js
+++ js/fullpage_code.js
@@ -3,14 +3,36 @@ $(function(){
 
 
   navigation:true,
-  fixedElements:'#header',
+  fixedElements:'#header, .overlay',
   verticalCentered:false,
   responsiveWidth:700,
-  anchors : ['1st','2nd','3rd'],
+  anchors : ['1st','2nd','3rd','4th'],
   afterLoad:function(anchorLink, index){
     if(anchorLink == '2nd'){
       $(".page_txt_box .main_txt").addClass('txt');
       $(".page_txt_box .page_logo").addClass('txt_up');
+    }
+    if(anchorLink == '3rd'){
+       $(".page_title").addClass("slide_up_1");
+       $(".page_sub_title").addClass("slide_up_2");
+       $(".slides").addClass("slide_up_3");
+    }
+    if(anchorLink =='3rd'|| anchorLink == '4th'){
+      $(".header .topnav a").css({color:"#000"});   
+      $(".topnav, .hidden_sub").on("mouseleave", function(){
+        $(".hidden_sub").stop().animate({height:0}, 300, function(){
+          $(".topnav li a").css({color:"#000"});
+          $(".hidden_sub").css({display:"none"});
+        });
+      });
+    }else{
+      $(".topnav li a").css({"color":"#fff"});
+      $(".topnav, .hidden_sub").on("mouseleave", function(){
+        $(".hidden_sub").stop().animate({height:0}, 300, function(){
+          $(".topnav li a").css({color:"#fff"});
+          $(".hidden_sub").css({display:"none"});
+        });
+      });
     }
   }
  });
--- js/jq.main.js
+++ js/jq.main.js
@@ -78,4 +78,63 @@ $(function(){
     }
   });
   
+
+  //Slide Functions
+  function resSize(autoLength) {
+    const liWidth = $(".slide_li").width() * autoLength;
+    const liLength = $(".slide_li").width() * $(".slide_li").     length;
+    $(".slide_box, .slides").width(liWidth);
+    $(".slide_inner").width(liLength);
+    //console.log(liWidth);
+  }
+  $(window).resize(function () {
+    const wWidth = $(window).width();
+    if (wWidth >= 700) {
+      resSize(3);
+    } else if (wWidth < 700) {
+      resSize(2);
+    }
+  });
+
+  if (matchMedia("screen and (min-width:800px").matches){
+    resSize(3);
+  } else if (matchMedia("screen and (max-width:799px").matches){
+    resSize(2);
+  }
+
+  function slideInit(){
+    let slideCount = 0;
+    const liWidth = $(".slide_li").width();
+    const slideSize = $(".slide_box").width() / $(".slide_li").width();
+    const slideLength = $(".slide_li").length - slideSize;
+
+    //console.log(slideSize);  --3
+
+
+
+
+    $(".arr_next").click(function(){
+      slideCount++;
+      if(slideCount > slideLength){
+        slideCount = 0; 
+      }
+
+      $(".slide_inner").stop().animate({"margin-left" : slideCount * -liWidth}, 250);
+    });
+
+    $(".arr_prev").click(function(){
+      slideCount--;
+      if(slideCount < 0){
+        slideCount = slideLength;
+      }
+      $(".slide_inner").stop().animate({"margin-left" : slideCount * -liWidth}, 250);
+    });
+  }
+  $(window).resize(function(){
+    $(".slider_inner").stop().animate({"margin-left":0}, 100);
+    slideInit();
+  });
+
+  slideInit();
+
 });
